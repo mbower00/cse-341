a2772eebb72613cca61d643d4b1002fda649c016
mbower00 <mitchbbower@gmail.com>
2024-04-26 19:39:25 -0600

    adding lesson 1 assignment code

---
+++ server.js
@@ -0,0 +1,13 @@
+// copied from https://codeforgeek.com/express-nodejs-tutorial/
+const express = require('express');
+const app = express();
+ 
+app.get('/', (req, res) => {
+  res.send("Alex Bower");
+});
+
+const port = 3000
+ 
+app.listen(process.env.PORT || port, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || port));
+});
