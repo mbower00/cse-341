215b22d363c1d89214b21b58f2f88c3156a85c74
mbower00 <mitchbbower@gmail.com>
2024-05-04 18:22:25 -0600

    MAC implimented contacts and contact routes

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,30 +1,40 @@
-// made using code from:
+// made using code from or used code from:
 //  the course video (https://www.youtube.com/watch?v=K00J87SofEc)
 //  https://www.coderrocketfuel.com/article/store-mongodb-credentials-as-environment-variables-in-nodejs
 //  https://stackoverflow.com/questions/58354629/moving-nodejs-mongodb-connection-code-to-another-file
 //  https://www.mongodb.com/blog/post/quick-start-nodejs-mongodb-how-to-get-connected-to-your-database
 //  https://www.mongodb.com/developer/languages/javascript/node-crud-tutorial/
-
-// https://www.mongodb.com/docs/manual/reference/method/db.collection.find/
-// https://www.mongodb.com/docs/manual/reference/method/cursor.toArray/
-
-
-const { MongoClient } = require('mongodb');
-const mongo = require('mongodb')
-const client = new MongoClient(process.env.CONNECTION_STRING)
+//  https://www.mongodb.com/docs/manual/reference/method/db.collection.find/
+//  https://www.mongodb.com/docs/manual/reference/method/cursor.toArray/
+//  https://stackoverflow.com/questions/6912584/how-to-get-get-query-string-variables-in-express-js-on-node-js?rq=1
 
 const contactsRoute = async (req, res) => {
+    const mongo = require('mongodb')
+    const client = new mongo.MongoClient(process.env.CONNECTION_STRING)
     await client.connect()
-    const cseDb = db.db('cse-341')
-    const contactsCollection = cseDb.collection('contacts')
-    res.send(await listDatabases(client))
+
+    const cur = client.db('cse-341').collection('contacts').find()
+    const data = await cur.toArray()
+
+    res.send(data)
+    
+    await client.close()
 }
 
 const contactRoute = async (req, res) => {
+    const mongo = require('mongodb')
+    const client = new mongo.MongoClient(process.env.CONNECTION_STRING)
     await client.connect()
-    const cseDb = db.db('cse-341')
-    const contactsCollection = cseDb.collection('contacts')
-    res.send(contactsCollection)
+
+    const data = await client.db('cse-341').collection('contacts').findOne({_id: new mongo.ObjectId(req.query.id)})
+
+    if (data) {
+        res.send(data)
+    } else {
+        res.send('NO CONTACT FOUND')
+    }
+    
+    await client.close()
 }
 
 module.exports = {
